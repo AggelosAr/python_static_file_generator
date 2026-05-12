6646eec91441e22e1f37e265193d2813e97f0984
AggelosAr <aggelosarelakis@gmail.com>
2026-05-12 17:39:18 +0300

    move comments at the top

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,9 +1,11 @@
-# Todo for tag assert it doesnt conttain < and > 
 
+# Todo for tag assert it doesnt conttain < and > 
+# Todo maybe add comparison method.
+# todo add an enum of tags or smth. And validate against them. is this None?
 class HTMLNode:
 
     def __init__(self, 
-                 tag: str, # todo add an enum of tags or smth. And validate against them. is this None?
+                 tag: str, 
                  value: str | None = None, 
                  children: list['HTMLNode'] = None, 
                  props: dict | None = None):
@@ -16,8 +18,6 @@ class HTMLNode:
     # Todo add tests for this method.
     def to_html(self):
         raise NotImplementedError
-    
-    # Todo maybe add comparison method.
 
     def props_to_html(self) -> str:
         return ''.join([f' {k}="{v}"' for k, v in self.props.items()])
