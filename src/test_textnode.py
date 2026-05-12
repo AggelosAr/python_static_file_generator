8276a0ed92eb61268862aa0799e003e4d52f6747
AggelosAr <aggelosarelakis@gmail.com>
2026-05-12 05:08:18 +0300

    Add tests for TextNode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -8,6 +8,28 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD_TEXT)
         node2 = TextNode("This is a text node", TextType.BOLD_TEXT)
         self.assertEqual(node, node2)
+    
+    def test_not_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD_TEXT)
+        node2 = TextNode("This is a text node", TextType.ITALIC_TEXT)
+        self.assertNotEqual(node, node2)
+
+    def test_instatate_works(self):
+        text_node = TextNode(text="This is some anchor text",
+                             text_type="link",
+                             url="https://www.boot.dev")
+        self.assertEqual(text_node.text, "This is some anchor text")
+        self.assertEqual(text_node.text_type, "link")
+        self.assertEqual(text_node.url, "https://www.boot.dev")
+    
+    def test_instatate_works_with_none_url(self):
+        text_node = TextNode(text="This is some anchor text",
+                             text_type="link")
+        self.assertEqual(text_node.text, "This is some anchor text")
+        self.assertEqual(text_node.text_type, "link")
+        self.assertEqual(text_node.url, None)
+
+
 
 
 if __name__ == "__main__":
