e8d2ffc982b269e04065704dfa32458a65f5edf1
AggelosAr <aggelosarelakis@gmail.com>
2026-05-12 17:20:51 +0300

    finish leaf node class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -29,21 +29,32 @@ class HTMLNode:
         return self.__str__()
 
 
-# TODO add tests for this class
 class LeafNode(HTMLNode):
 
     def __init__(self, 
                  value: str,
-                 tag: str | None = None,
+                 tag: str | None,
                  props: dict | None = None):
         
         super().__init__(tag=tag, value=value, props=props)
 
     def to_html(self) -> str:
-        ...
+        if self.value is None:
+            raise ValueError('All leaf nodes must have a value.')
+        if self.tag is None:
+            return str(self.value)
+        
+        props = ' '.join([f'{k}="{v}"' for k, v in self.props.items()])
+        tag_start, tag_close, html_tag_close = '<', '>', '/'
+
+        start = f'{tag_start}{self.tag} {props}{tag_close}'
+        middle = f'{self.value}'
+        end = f'{tag_start}{html_tag_close}{self.tag}{tag_close}'
+
+        return f'{start}{middle}{end}'
     
     def __str__(self):
-        ...
+        return f'LeafNode({self.tag}, {self.value}, {self.props})'
     
     def __repr__(self):
         return self.__str__()
