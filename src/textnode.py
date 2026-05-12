1de29617c409abff6778812602509cd852bb2fa0
AggelosAr <aggelosarelakis@gmail.com>
2026-05-12 03:40:38 +0300

    Finish text node -> add init , eq, rerp

--- src/textnode.py
+++ src/textnode.py
@@ -7,3 +7,18 @@ class Bender(Enum):
     CODE_TEX = "code_text"
     LINK = "link"
     IMAGE = "image" 
+
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+    
+    def __eq__(self, other: 'Bender'):
+        return all(
+            self.text == other.text,
+            self.text_type == other.text_type,
+            self.url == other.url,
+        )
+    
+    def __repr__(self):
+        return f'TextNode({self.text}, {self.text_type}, {self.url})'
