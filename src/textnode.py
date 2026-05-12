2f2550862429f2db5db6cb910649c7c880dfcd19
AggelosAr <aggelosarelakis@gmail.com>
2026-05-12 03:34:03 +0300

    add python enums for all inline text types

---
+++ src/textnode.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class Bender(Enum):
+    TEXT = "text"
+    BOLD_TEXT = "bold_text"
+    ITALIC_TEXT = "italic_text"
+    CODE_TEX = "code_text"
+    LINK = "link"
+    IMAGE = "image" 
