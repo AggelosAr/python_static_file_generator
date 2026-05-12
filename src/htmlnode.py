e486ecfeed7d15903c3376b529feb9da6ac3af16
AggelosAr <aggelosarelakis@gmail.com>
2026-05-12 05:47:51 +0300

    Add html node

---
+++ src/htmlnode.py
@@ -0,0 +1,29 @@
+
+
+class HTMLNode:
+
+    def __init__(self, 
+                 tag: str, # todo add an enum of tags or smth. And validate against them.
+                 value: str, 
+                 children: list['HTMLNode'] = None, 
+                 props: dict | None = None):
+        
+        self.tag = tag 
+        self.value = value
+        self.children = children
+        self.props = props
+
+    # Todo add tests for this method.
+    def to_html(self):
+        raise NotImplementedError
+    
+    def props_to_html(self):
+        if not self.props:
+            return ''
+        return ''.join([f' {k}="{v}"' for k, v in self.props.items()])
+
+    def __str__(self):
+        return f'TextNode({self.text}, {self.text_type}, {self.url})'
+    
+    def __repr__(self):
+        return f'TextNode({self.text}, {self.text_type}, {self.url})'
