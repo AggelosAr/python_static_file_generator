55791ee434810bbf2c994f8104854473028e6c12
AggelosAr <aggelosarelakis@gmail.com>
2026-05-12 19:14:32 +0300

    add tests for str reprt and minor refactor of tests

--- src/test_html_node.py
+++ src/test_html_node.py
@@ -6,49 +6,51 @@ from nodes.html_node import HTMLNode
 class TestHTMLNode(unittest.TestCase):
 
     def get_sample_test_html_node_no_props(self) -> HTMLNode:
-        html_node = HTMLNode(tag='p',
-                             value='Some random paragraph!')
+        html_node = HTMLNode(tag='p', value='Some random paragraph!')
         return html_node
-    
-    def test_instantiate_works_no_children(self):
-        html_node = HTMLNode(tag='p',
-                             value='Some random paragraph!',
-                             props={'class': 'container'})
-        
-        self.assertEqual(html_node.tag, 'p')
-        self.assertEqual(html_node.value, 'Some random paragraph!')
-        self.assertEqual(html_node.props, {'class': 'container'})
 
-    def test_instantiate_works_with_nested_list_children(self):
-
-        list_item1 = HTMLNode(tag='li',
-                              value='First item')
-
-        list_item2 = HTMLNode(tag='li',
-                              value='Second item')
-
-        ordered_list = HTMLNode(tag='ol',
-                                children=[list_item1, list_item2])
+    def get_sample_test_html_node_with_children(self) -> HTMLNode:
+        list_item1 = HTMLNode(tag='li', value='First item')
+        list_item2 = HTMLNode(tag='li', value='Second item')
+        ordered_list = HTMLNode(tag='ol', children=[list_item1, list_item2])
 
         html_node = HTMLNode(tag='div',
                              children=[ordered_list],
                              props={'class': 'container'})
-
-        self.assertEqual(html_node.tag, 'div')
-        self.assertEqual(html_node.props, {'class': 'container'})
-
-        self.assertEqual(len(html_node.children), 1)
-        self.assertEqual(html_node.children[0].tag, 'ol')
-
-        self.assertEqual(html_node.children[0].children,
-                         [list_item1, list_item2])
-
-        self.assertEqual(html_node.children[0].children[0].value,
-                         'First item')
-
-        self.assertEqual(html_node.children[0].children[1].value,
-                         'Second item')
+        return html_node
     
+    def test_instantiate_works_no_children(self):
+        props = {}
+        html_node = self.get_sample_test_html_node_no_props()
+        html_node.props = props
+        
+        self.assertEqual('p', html_node.tag)
+        self.assertEqual('Some random paragraph!', html_node.value)
+        self.assertEqual({}, html_node.props)
+
+    def test_instantiate_works_with_nested_list_children(self):
+        html_node = self.get_sample_test_html_node_with_children()
+
+        self.assertEqual('div', html_node.tag)
+        self.assertEqual({'class': 'container'}, html_node.props)
+
+        self.assertEqual(2, len(html_node.children))
+        self.assertEqual('ol', html_node.children[0].tag)
+
+        self.assertEqual('li', html_node.children[0].children[1].value)
+        self.assertEqual('First item', html_node.children[0].children[0].value)
+        self.assertEqual('li', html_node.children[1].children[1].value)
+        self.assertEqual('Second item', html_node.children[1].children[0].value)
+    
+    def test_to_html_raises_exc(self):
+        html_node = self.get_sample_test_html_node_no_props()
+
+        with self.assertRaises(NotImplementedError) as context:
+            _ = html_node.to_html()
+        
+        self.assertEqual('Only children of this class should implement this method!', 
+                         str(context.exception))
+
     def test_props_to_html_none(self):
         props = {}
         html_node = self.get_sample_test_html_node_no_props()
@@ -75,6 +77,38 @@ class TestHTMLNode(unittest.TestCase):
 
         self.assertEqual(' href="https://www.google.com" target="_blank"', 
                          html_node.props_to_html())
+
+    def test___str__with_no_children_no_props(self):
+        html_node = self.get_sample_test_html_node_no_props()
+        expected = 'HTMLNode(p, Some random paragraph!, children: None, None)'
+
+        self.assertEqual(expected, html_node.__str__())
+    
+    def test___str__with_nested_children_and_props(self):
+        props = {'class': 'container'}
+        html_node = self.get_sample_test_html_node_with_children()
+        html_node.props = props
+
+        expected = "HTMLNode(div, None, children: [HTMLNode(ol, None, " \
+        "children: [HTMLNode(li, First item, children: None, None), " \
+        "HTMLNode(li, Second item, children: None, None)], None)], {'class': 'container'})"
+
+        self.assertEqual(expected, html_node.__str__())
+    
+    def test___str___with_no_children_and_props(self):
+        props = {'class': 'container'}
+        html_node = self.get_sample_test_html_node_no_props()
+        html_node.props = props
+
+        expected = "HTMLNode(p, Some random paragraph!, children: None, {'class': 'container'})"
+
+        self.assertEqual(expected, html_node.__str__())
+     
+    def test___repr__(self):
+        html_node = self.get_sample_test_html_node_no_props()
+        expected = 'HTMLNode(p, Some random paragraph!, children: None, None)'
+
+        self.assertEqual(expected, html_node.__str__())
 
 
 if __name__ == '__main__':
