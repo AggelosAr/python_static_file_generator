488d8c29714a60cfd8f421450aac86ff6eb29bbf
AggelosAr <aggelosarelakis@gmail.com>
2026-05-14 10:04:19 +0300

    fix urls in md

--- src/main.py
+++ src/main.py
@@ -8,6 +8,15 @@ def main():
 
     generate_page(from_path='content/blog/glorfindel/index.md',
                   dest_path='public/glorfindel.html')
+    
+    generate_page(from_path='content/blog/tom/index.md',
+                  dest_path='public/tom.html')
+
+    generate_page(from_path='content/blog/majesty/index.md',
+                  dest_path='public/majesty.html')
+    
+    generate_page(from_path='content/contact/index.md',
+                  dest_path='public/contact.html')
 
 if __name__ == '__main__':
     main()
